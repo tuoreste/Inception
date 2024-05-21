d16c6cf273e44739b1bb2394f07b0f22b90945a9
Oreste <tuoreste@gmail.com>
2024-05-21 08:04:36 +0200

    wordpress day

---
+++ requirements/wordpress/tools/setup_wp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+WORDPRESS_DIR = "/var/www/html/wordpress"
+PHP_FPM_CONF = "php-fpm.conf"
+WORDPRESS_DOWNLOAD_URL = "https://wordpress.org/latest.tar.gz"
+WORDPRESS_ARCHIVE = "latest.tar.gz"
+
+#check if WordPress is already downloaded
