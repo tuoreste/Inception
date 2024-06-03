b44d6eb133c2789795299adcc3d9940fe98f5329
Oreste <tuoreste@gmail.com>
2024-06-03 13:27:29 +0200

    Fix: mariadb running together with wp & nginx huleee

--- srcs/requirements/mariadb/tools/setup_mdb.sh
+++ srcs/requirements/mariadb/tools/setup_mdb.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-set -o allexport
-source ./requirements
-set +o allexport
+mysql_install_db
+mysqld
+# """
+# mariadb_init_file="/tools/mdb_init.sql"
+# bootstrap_file="/tools/bootstrap.sql"
 
-mariadb_init_file="/tools/mdb_init.sql"
-bootstrap_file="/tools/bootstrap.sql"
+# run_bootstrap() {
+# 	cat <<EOF > "$bootstrap_file"
+# CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+# GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+# FLUSH PRIVILEGES;
+# EOF
+# 	mariadbd --user=mysql --bootstrap < "$bootstrap_file"
+# 	rm -f "$bootstrap_file"
+# }
 
-run_bootstrap() {
-	cat <<EOF > "$bootstrap_file"
-CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-FLUSH PRIVILEGES;
-EOF
-	mariadbd --user=mysql --bootstrap < "$bootstrap_file"
-	rm -f "$bootstrap_file"
-}
+# database_exists=$(mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "SHOW DATABASES LIKE '${MYSQL_DATABASE}';" | grep "${MYSQL_DATABASE}")
 
-database_exists=$(mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "SHOW DATABASES LIKE '${MYSQL_DATABASE}';" | grep "${MYSQL_DATABASE}")
+# if [ -z "$database_exists" ]; then
+# 	run_bootstrap
+# fi
 
-if [ -z "$database_exists" ]; then
-	run_bootstrap
-fi
-
-mkdir -p /run/mysqld
-chown mysql:mysql /run/mysqld
+# mkdir -p /run/mysqld
+# chown mysql:mysql /run/mysqld
 
 # exec /usr/bin/mariadbd --user=mysql --datadir="/var/lib/mysql" --init-file="$mariadb_init_file"
-exec mysqld_safe
+# # exec mysqld_safe
+# """
